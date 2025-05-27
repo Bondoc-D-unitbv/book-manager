cc245906f9ccfc2fb67e6f737f233a96872c8c68
Bondoc Daniel <daniel.bondoc.it@gmail.com>
2025-05-27 20:35:06 +0300

    fix preloading data for edit function

--- src/app/features/book-form/book-form.component.ts
+++ src/app/features/book-form/book-form.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from "@angular/core";
-import { FormGroup } from "@angular/forms";
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, Inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NZ_MODAL_DATA } from 'ng-zorro-antd/modal';
+import { CommonModule } from '@angular/common';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-book-form',
   templateUrl: 'book-form.component.html',
   styleUrls: ['book-form.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NzInputModule
+  ]
 })
-export class BookFormComponent { 
-  @Input() form!: FormGroup;
+export class BookFormComponent {
+  form: FormGroup;
+
+  constructor(@Inject(NZ_MODAL_DATA) public data: { form: FormGroup }) {
+    this.form = data.form;
+  }
 }
